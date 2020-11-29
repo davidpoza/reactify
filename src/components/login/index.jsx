1153287c2d2136d20a5e6fff83f36c74abff3ca2
David Poza Suárez <pozasuarez@gmail.com>
2020-11-29 16:18:50 +0100

    added login submit and disable button if no pass or no email

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
@@ -8,17 +8,28 @@ import useStyles from './styles';
 
 function Login() {
   const classes = useStyles();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function onFormSubmit(e) {
+    e.preventDefault();
+    console.log("formulario enviado")
+  }
+
   return (
     <Grid container direction="column" justify="center" alignItems="center" style={{ minHeight: '100vh' }}>
       <Grid item xs={12} lg={5} className={classes.root}>
-        <form noValidate autoComplete="off">
+        <form noValidate autoComplete="off" onSubmit={onFormSubmit}>
           <Typography variant="h1" className={classes.title}>Reactify</Typography>
           <p>
-            <TextField id="email" label="Email" variant="outlined" className={classes.email} />
-            <TextField id="password" type="password" label="Password" variant="outlined" />
+            <TextField
+              id="email" label="Email" variant="outlined" className={classes.email}
+              value={email} onChange={(e) => setEmail(e.target.value)} />
+            <TextField id="password" type="password" label="Password" variant="outlined"
+              value={password} onChange={(e) => setPassword(e.target.value)} />
           </p>
           <p>
-            <Button variant="contained" color="primary" disableElevation>
+            <Button variant="contained" color="primary" type="submit" disableElevation disabled={!password || !email}>
               Login
             </Button>
           </p>
