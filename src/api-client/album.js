8a8146e643d26ef2805f9955cc9e4569c47e6ec1
David Poza Suárez <pozasuarez@gmail.com>
2020-12-06 20:26:44 +0100

    added search by song title

--- src/api-client/album.js
+++ src/api-client/album.js
@@ -23,12 +23,40 @@ export async function getAlbum({
   }
 };
 
-
+/**
+ * Returns songs for specified album
+ * @param {Object} params
+ * @param {string} params.token - jwt token
+ * @param {number} params.albumId - album id
+ */
 export async function getAlbumSongs({
   token, albumId
 }) {
   try {
-    const res = await fetch(`${config.API_HOST}/songs?albums.id=${albumId}`, {
+    const res = await fetch(`${config.API_HOST}/songs?albums_eq=${albumId}`, {
+      method: 'GET',
+      headers: {
+        "Authorization": "Bearer "+token,
+
+      },
+    });
+    return await res.json();
+  } catch {
+    return -1
+  }
+};
+
+/**
+ * Fetches songs with text included in name
+ * @param {Object} params
+ * @param {string} params.token - jwt token
+ * @param {number} params.text - text included in song name
+ */
+export async function searchSong({
+  token, text
+}) {
+  try {
+    const res = await fetch(`${config.API_HOST}/songs?name_contains=${text}`, {
       method: 'GET',
       headers: {
         "Authorization": "Bearer "+token,
