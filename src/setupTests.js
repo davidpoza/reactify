9e4128b51f5dcd31f4a3d7645d6ad4baadb9b923
David Poza Suárez <pozasuarez@gmail.com>
2020-12-18 19:39:42 +0100

    added jest and enzyme

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,7 @@
 import 'jest-enzyme';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import '@babel/polyfill';
 
 // Configuramos Enzyme
 configure({ adapter: new Adapter() });
