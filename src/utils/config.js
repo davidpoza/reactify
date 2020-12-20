610ae0743f1817363cd3d3101550327aa432cd61
David Poza Suárez <pozasuarez@gmail.com>
2020-12-20 13:16:46 +0100

    changed api downloader url to temporal one

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
   API_HOST: 'https://reactify.davidinformatico.com',
-  API_DOWNLOADER_HOST: 'http://localhost:5000',
+  API_DOWNLOADER_HOST: 'http://files.davidinformatico.com:5000',
   drawerWidth: 240,
 };
 
