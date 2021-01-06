870501aa64ea5d78805b8a70b8c5a5271a6bac0d
David Poza Suárez <pozasuarez@gmail.com>
2021-01-06 18:44:04 +0100

    added pipeline and changed api url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_HOST: 'https://reactify.davidinformatico.com',
+  API_HOST: 'https://reactify.api.davidinformatico.com',
   API_DOWNLOADER_HOST: 'http://files.davidinformatico.com:5000',
   drawerWidth: 240,
 };
